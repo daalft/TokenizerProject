df06699e00f3c101c9a061a8fd1a77b8995e1b62
David Alfter <alfter.david@gmx.net>
2011-12-08 15:43:07 +0100

    Commit 8 December 2011.

--- lib/tokenizer/opt_parser.rb
+++ lib/tokenizer/opt_parser.rb
@@ -7,21 +7,38 @@ module Tokenizer
 		def self.parse args
 			@@options = {}
 			parser = create_parser #implizit : OptParser.create_parser
-			parser.parse(args)
+			begin
+				parser.parse(args)
+			rescue OptionParser::InvalidArgument
+			rescue OptionParser::InvalidOption => e
+				
+				STDERR.puts "The system has encountered the following error:\n#{e.message}"
+				STDERR.puts "You have used an invalid option on the tokenizer."
+				STDERR.puts "To view the help and a list of valid options\ntype \"tokenize -h\""
+				exit 1
+			end
 		end
 		
 		private
 		
 		def self.create_parser
 			OptionParser.new do |args|
+			
 				args.banner = 'Usage: tokenize ARGS'
+			
 				args.on('-h', '--help', 'Show this summary!') do 
 					puts args
 					exit
 				end
 				args.on('-v', '--version', 'Show the version!') do
-					puts "Tokenizer version #{Tokenizer::VERSION} written and copyrighted by David Alfter."
+					puts "Tokenizer version #{VERSION} written and copyrighted by David Alfter."
+					exit
 				end
+				#read filename, make absolute path, existence test, readability test, fehlermeldungen liefern (separat) ( + test)
+				args.on('-f', '--file', 'Read filename') do
+					
+				end
+			
 			end
 		end
 	end #class
